8e17419c85eae2167795d3a0f12069a15b42ed0e
rouzbeh <rouzyd@gmail.com>
2021-03-11 18:39:15 +0330

    added basic screen

--- main.py
+++ main.py
@@ -5,6 +5,17 @@ class Calculator:
         self.master = master
         master.title("Python Calculator")
 
+        # create screen widget
+        self.screen = Text(master, state='disabled', width=30, height=3, background="grey", foreground="blue")
+
+        # position screen in window
+        self.screen.grid(row=0, column=0, columnspan=4, padx=5, pady=5)
+        self.screen.configure(state='normal')
+
+        # initialize screen value as empty
+        self.equation = ''
+
+
 root = Tk()
 main_gui = Calculator(root)
 root.mainloop()
